575158e562abd567a22a71cea7122248d50b4a51
wsc0der <ws.coder@outlook.com>
2025-06-17 11:51:18 +0800

    Open nvim-tree by default when open nvim in a folder

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,9 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argv(0) == '.' then
+      require("nvim-tree.api").tree.open()
+    end
+  end
+})
